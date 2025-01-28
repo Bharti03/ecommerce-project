7ff637670a7ee003da6d5d82a46335c1fb34799c
bharti35 <bharti03500@gmail.com>
2025-01-28 11:13:03 +0530

    Initial commit for the ecommerce project

---
+++ src/App.js
@@ -0,0 +1,20 @@
+import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Navbar from "./components/Navbar";
+import Shop from "./pages/shop/shop";
+import Cart from "./pages/cart/cart";
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Shop />}></Route>
+          <Route path="/cart" element={<Cart />}></Route>
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
